ce26343f8c2544eaabdf64d4939ec9edbb120165
Ruslan Novikov <dib.08@mail.ru>
2025-10-06 16:46:43 +0300

    feat: move all console operations to InputManager

--- InputManager.cs
+++ InputManager.cs
@@ -2,28 +2,25 @@
 {
     internal class InputManager
     {
-        private readonly Calculator calculator;
-        public InputManager(Calculator calculator)
-        {
-            this.calculator = calculator;
-        }
 
-        public static double ReadValidNumber()
+        public double ReadValidNumber(string message)
         {
-
+            Console.Write(message);
             while (true)
             {
-                string value= Console.ReadLine();
+                string value = Console.ReadLine();
                 if (double.TryParse(value, out double convertedNumber))
                 {
                     return convertedNumber;
                 }
-                    Console.Write("введите корректное число :");
-                }
+                Console.Write("введите корректное число :");
+            }
         }
-        public static string ReadValidOperation(string[] allowedOperations)
+        public string ReadValidOperation(string[] allowedOperations, string message)
         {
-            while(true)
+
+            Console.Write(message);
+            while (true)
             {
                 string inputValue = Console.ReadLine();
                 if (allowedOperations.Contains(inputValue))
@@ -31,69 +28,38 @@
                     return inputValue;
                 }
                 Console.Write("данной операции не существует. Введите корректную операцию! :");
-                
+
             }
         }
-        public static bool ReadUsersAnswer()
+        public bool ReadUsersAnswer(string message)
         {
+            Console.Write(message);
             string userAnswer = Console.ReadLine();
             if (userAnswer.ToLower() == "да") return true;
             return false;
         }
-        public static void PrintOperations(string[] inputOperations)
+        public void PrintOperations(string[] inputOperations)
         {
-            foreach(var i in inputOperations)
+            Console.WriteLine();
+            foreach (var i in inputOperations)
             {
                 Console.WriteLine(i);
             }
         }
-        public void Run()
+        public void DisplayTitle() => Console.WriteLine("Калькулятор 3000");
+        public void DisplayResult(double firstOperand, string operation, double secondOperand, double result)
+        => Console.WriteLine($"\nРезультат вычисления: {firstOperand} {operation} {secondOperand} = {result}");
+
+        public void DisplayError(string message)
+        => Console.WriteLine(message);
+
+        public void PromptToContinue()
         {
-            string[] operationsList = { "+", "-", "/", "*" };
-            bool isExitApp = true;
-            while (isExitApp)
-            {
-                Console.WriteLine("Калькулятор 3000");
-                try
-                {
-                    Console.Write("\nвведите первый операнд:");
-
-                    double inputFirstOperand = ReadValidNumber();
-
-                    Console.WriteLine("\nвыберите операцию из перечисленных");
-
-                    PrintOperations(operationsList);
-
-                    Console.Write(":");
-                    string operation = ReadValidOperation(operationsList);
-
-                    Console.Write("\nвведите второй операнд:");
-
-                    double inputSecondOperand = ReadValidNumber();
-
-                    double resultCalculating = calculator.Calculate(operation, inputFirstOperand, inputSecondOperand);
-
-                    Console.WriteLine($"\nРезультат вычисления: {inputFirstOperand} {operation} {inputSecondOperand} = {resultCalculating}");
-
-                }
-                catch (DivideByZeroException divideException)
-                {
-                    Console.WriteLine(divideException.Message);
-                }
-                catch (Exception ex)
-                {
-                    Console.WriteLine(ex.Message);
-                }
-
-                Console.Write("\nхотите продолжить вычисления? \n(да/нет) :");
-
-                isExitApp = ReadUsersAnswer();
-
-                Console.WriteLine("для продолжения нажмите на любую клавишу");
-                Console.ReadKey();
-                Console.Clear();
-
-            }
+            Console.WriteLine("Для продолжения нажмите любую клавишу");
+            Console.ReadKey();
         }
+
+        public void ClearConsole()
+        => Console.Clear();
     }
 }
--- Program.cs
+++ Program.cs
@@ -4,11 +4,47 @@
     {
         static void Main(string[] args)
         {
-            CalculatorOperations calculatorOperations=new();
-            Calculator calculator = new(calculatorOperations);
-            InputManager inputManager = new(calculator);
-            inputManager.Run();
+            Run();
         }
-       
+        static void Run()
+        {
+            CalculatorOperations calculatorOperations = new();
+            Calculator calculator = new(calculatorOperations);
+            InputManager inputManager = new();
+            string[] operationsList = { "+", "-", "/", "*" };
+            bool isExitApp = true;
+            while (isExitApp)
+            {
+                inputManager.DisplayTitle();
+                try
+                {
+                    double inputFirstOperand = inputManager.ReadValidNumber("\nвведите первый операнд:");
+
+                    inputManager.PrintOperations(operationsList);
+
+                    string operation = inputManager.ReadValidOperation(operationsList, "\nвыберите операцию из перечисленных выше:");
+
+                    double inputSecondOperand = inputManager.ReadValidNumber("\nвведите второй операнд:");
+
+                    double resultCalculating = calculator.Calculate(operation, inputFirstOperand, inputSecondOperand);
+
+                    inputManager.DisplayResult(inputFirstOperand, operation, inputSecondOperand, resultCalculating);
+                }
+                catch (DivideByZeroException divideException)
+                {
+                    inputManager.DisplayError(divideException.Message);
+                }
+                catch (Exception ex)
+                {
+                    inputManager.DisplayError(ex.Message);
+                }
+
+                isExitApp = inputManager.ReadUsersAnswer("\nхотите продолжить вычисления? \n(да/нет) :");
+
+                inputManager.PromptToContinue();
+                inputManager.ClearConsole();
+
+            }
+        }
     }
 }
